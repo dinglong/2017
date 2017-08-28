ffa2e01cfb1c2fe1639eab79fa4f7b157b95b7c6
longlong <gnolld@foxmail.com>
2017-08-28 17:22:06 +0800

    modify format

--- study/src/mymath/mymath.go
+++ study/src/mymath/mymath.go
@@ -1,12 +1,12 @@
 package mymath
 
 func Add(a, b int) int {
-	return a + b
+    return a + b
 }
 func Max(a, b int) (ret int) {
-	ret = a
-	if b > a {
-		ret = b
-	}
-	return
+    ret = a
+    if b > a {
+        ret = b
+    }
+    return
 }
