4c903cd122f391076baaef10657b3cb18a0d020c
longlong <gnolld@foxmail.com>
2017-08-29 18:00:29 +0800

    Add notification listener service, test registry notification

---
+++ authserver/src/anthserver/restserver/services/notification.go
@@ -0,0 +1,42 @@
+package services
+
+import (
+	"log"
+
+	"github.com/emicklei/go-restful"
+	"io/ioutil"
+	"net/http"
+)
+
+type EventService struct {
+	name string
+}
+
+func NewEventService() (service EventService) {
+	service = EventService{
+		name: "EventService",
+	}
+	return service
+}
+
+func (e EventService) Name() string {
+	return e.name
+}
+
+func (e EventService) Register(webService *restful.WebService) {
+	webService.Route(webService.POST("/event").To(revcEvent))
+	log.Println("hello service regist subpath /event")
+}
+
+func revcEvent(req *restful.Request, resp *restful.Response) {
+	log.Printf("event function req [%v]\n", req)
+
+	// print body
+	if data, err := ioutil.ReadAll(req.Request.Body); err != nil {
+		log.Printf("event function read req body error %v\n", err)
+	} else {
+		log.Printf("event function read body [%v]\n", string(data))
+	}
+
+	resp.WriteHeader(http.StatusOK)
+}
